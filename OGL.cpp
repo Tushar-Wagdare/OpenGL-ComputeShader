314b3ae0d20369244136e4b8ac6a9a5cbf561fa9
Tushar-Wagdare <rahastugawa@gmail.com>
2025-01-31 18:31:31 +0530

    Added -> Compute shader is added ...

---
+++ OGL.cpp
@@ -0,0 +1,354 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <gl/GL.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "WindowManager.h"
+#include "Logger.h"
+#include "Timer.h"
+#include "camera.h"
+#include "Shader.h"
+#include "model.h"
+
+
+
+//*** Globle Function Declarations ***
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void renderQuad();
+
+
+//*** Global Variable Declaration ***
+WindowManager* pWindow = NULL;
+Camera* camera = NULL;
+float lastX = WindowManager::SCR_WIDTH / 2.0f;
+float lastY = WindowManager::SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+// texture size
+const unsigned int TEXTURE_WIDTH = 1000, TEXTURE_HEIGHT = 1000;
+
+
+///==================== OpenGL Variables =======================///
+GLuint vaoCube = 0;
+GLuint vbo_position_cube = 0;
+GLuint vbo_normal_cube = 0;
+
+GLfloat angleCube = 0.0f;
+// lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+
+
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	unsigned int loadTexture(char const* path);
+	pWindow = new WindowManager();
+	MSG msg = { 0 };
+	camera = new Camera();
+
+	Logger::Init();
+
+	TIMER_INIT("Window");
+	pWindow->initialize();
+	TIMER_END(); 
+	LOG_INFO("Window Initialized in %.6f seconds", TIMER_GET("Window"));
+
+
+
+	///======================== OpenGL INIT ==============================///
+	int max_compute_work_group_count[3];
+	int max_compute_work_group_size[3];
+	int max_compute_work_group_invocations;
+
+	for (int idx = 0; idx < 3; idx++) {
+		glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_COUNT, idx, &max_compute_work_group_count[idx]);
+		glGetIntegeri_v(GL_MAX_COMPUTE_WORK_GROUP_SIZE, idx, &max_compute_work_group_size[idx]);
+	}
+	glGetIntegerv(GL_MAX_COMPUTE_WORK_GROUP_INVOCATIONS, &max_compute_work_group_invocations);
+
+	std::cout << "OpenGL Limitations: " << std::endl;
+	std::cout << "maximum number of work groups in X dimension " << max_compute_work_group_count[0] << std::endl;
+	std::cout << "maximum number of work groups in Y dimension " << max_compute_work_group_count[1] << std::endl;
+	std::cout << "maximum number of work groups in Z dimension " << max_compute_work_group_count[2] << std::endl;
+
+	std::cout << "maximum size of a work group in X dimension " << max_compute_work_group_size[0] << std::endl;
+	std::cout << "maximum size of a work group in Y dimension " << max_compute_work_group_size[1] << std::endl;
+	std::cout << "maximum size of a work group in Z dimension " << max_compute_work_group_size[2] << std::endl;
+
+	std::cout << "Number of invocations in a single local work group that may be dispatched to a compute shader " << max_compute_work_group_invocations << std::endl;
+
+	// build and compile shaders
+	// -------------------------
+	Shader screenQuad("shaders/screenQuad.vs", "shaders/screenQuad.fs");
+	ComputeShader computeShader("shaders/computeShader.cs");
+
+	screenQuad.use();
+	screenQuad.setInt("tex", 0);
+
+	// Create texture for opengl operation
+	// -----------------------------------
+	unsigned int texture;
+
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, TEXTURE_WIDTH, TEXTURE_HEIGHT, 0, GL_RGBA, GL_FLOAT, NULL);
+
+	glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_READ_WRITE, GL_RGBA32F);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+
+	int fCounter = 0;
+
+
+	//*** Game LOOP ***
+	while (pWindow->isRunning == FALSE)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				pWindow->isRunning = TRUE;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			///=========================== DISPLAY ==================================//
+			// per-frame time logic
+			// --------------------
+			float currentFrame = static_cast<float>(Timer::getAppRunTime() * 0.05f);
+			deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
+
+
+			if (fCounter > 500) {
+				std::cout << "FPS: " << 1 / deltaTime << std::endl;
+				fCounter = 0;
+			}
+			else {
+				fCounter++;
+			}
+
+			computeShader.use();
+			computeShader.setFloat("t", currentFrame);
+			glDispatchCompute((unsigned int)TEXTURE_WIDTH / 10, (unsigned int)TEXTURE_HEIGHT / 10, 1);
+
+			// make sure writing to image has finished before read
+			glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+			// render image to quad
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			screenQuad.use();
+
+			renderQuad();
+
+
+			pWindow->swapDisplayBuffer();
+
+			///================== UPDATE =======================//
+			//angleCube = angleCube + 0.02f;
+
+		}
+	}
+
+
+	return((int)msg.wParam);
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//*** Function Declaration ***
+	void ToggleFullscreen(void);
+	void resize(int, int);
+
+
+	//*** Code ***
+	switch (iMsg)
+	{
+	case WM_SETFOCUS:
+		break;
+
+	case WM_KILLFOCUS:
+		break;
+
+	case WM_SIZE:
+        glViewport(0, 0, LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_ERASEBKGND:
+		return(0);
+
+	case WM_MOUSEMOVE:
+		{
+			float xpos = static_cast<float>(GET_X_LPARAM(lParam));
+			float ypos = static_cast<float>(GET_Y_LPARAM(lParam));
+
+			if (firstMouse)
+			{
+				lastX = xpos;
+				lastY = ypos;
+				firstMouse = false;
+			}
+
+			float xoffset = xpos - lastX;
+			float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+			lastX = xpos;
+			lastY = ypos;
+			camera->ProcessMouseMovement(xoffset, yoffset);
+		}
+		
+		break;
+
+	case WM_KEYDOWN:
+		switch (LOWORD(wParam))
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hwnd);
+			break;
+		}
+		break;
+
+	case WM_CHAR:
+		switch (LOWORD(wParam))
+		{
+		case 'F':
+		case 'f':
+			
+			break;
+
+		case 'W':
+		case 'w':
+			camera->ProcessKeyboard(FORWARD, deltaTime);
+			break;
+
+		case 'A':
+		case 'a':
+			camera->ProcessKeyboard(LEFT, deltaTime);
+			break;
+
+		case 'S':
+		case 's':
+			camera->ProcessKeyboard(BACKWARD, deltaTime);
+			break;
+
+		case 'D':
+		case 'd':
+			camera->ProcessKeyboard(RIGHT, deltaTime);
+			break;
+
+		
+		}
+		break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+
+unsigned int loadTexture(char const* path)
+{
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
+}
+
+
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+void renderQuad()
+{
+	if (quadVAO == 0)
+	{
+		float quadVertices[] = {
+			// positions        // texture Coords
+			-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+			-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+			 1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+			 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		};
+		// setup plane VAO
+		glGenVertexArrays(1, &quadVAO);
+		glGenBuffers(1, &quadVBO);
+		glBindVertexArray(quadVAO);
+		glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	}
+	glBindVertexArray(quadVAO);
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+	glBindVertexArray(0);
+}
+
